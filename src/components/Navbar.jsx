4b1d0982462b28fca3b0ed77b2a817d30b692368
Gregoire <gregoire.duhem@epitech.eu>
2022-04-08 11:37:10 +0200

    [+] github and linkedin link redirect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://www.linkedin.com/in/grégoire-duhem-7a98a5184/"
             >
               LinkedIn <FaLinkedin size={30} />
             </a>
@@ -57,7 +57,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/GregoireDuhem"
             >
               Github <FaGithub size={30} />
             </a>
