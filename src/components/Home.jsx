7716119784ebb5ddacab109039416ef3729ca444
Gregoire <gregoire.duhem@epitech.eu>
2022-04-08 11:28:07 +0200

    [+] adding mobile responsive navbar and social button

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return <div className="w-full h-screen bg-[#071120] "></div>;
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -10,32 +15,47 @@ const Navbar = () => {
       </div>
 
       {/* Menu */}
-      <div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Works</li>
-          <li>Contacts</li>
-        </ul>
-      </div>
-
-      {/* Hambuger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-
-      {/* Mobile Menu */}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Works</li>
         <li>Contacts</li>
       </ul>
-      {/* Social */}
 
-      <div></div>
+      {/* Hambuger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-7 text-4xl">Home</li>
+        <li className="py-7 text-4xl">About</li>
+        <li className="py-7 text-4xl">Skills</li>
+        <li className="py-7 text-4xl">Works</li>
+        <li className="py-7 text-4xl">Contacts</li>
+      </ul>
+
+      {/* Social */}
+      <div className="flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              LinkedIn <FaLinkedin size="30" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
